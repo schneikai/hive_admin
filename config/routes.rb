30ff3ba36f71813590d9a2084e7b0a2c570c7108
Kai Schneider <schneikai@gmail.com>
2015-02-20 11:03:24 +0100

    move routes to host app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
+  # TODO KS: Had to remove this and move to host app. If you have a catch-all
+  # route in host app and because Gem routes are loaded after host app this
+  # routes here would never get hit.
+  # ActiveAdmin.routes(self)
 end
